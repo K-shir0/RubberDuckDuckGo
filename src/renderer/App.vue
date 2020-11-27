1baf8158d2aad82287b2542954f1a0d9233fe504
K-shir0 <kakishiro2190394@gmail.com>
2020-11-28 01:21:45 +0900

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <landing-page></landing-page>
+  </div>
+</template>
+
+<script>
+  import LandingPage from '@/components/LandingPage'
+
+  export default {
+    name: 'rubber-duck-duck-go',
+    components: {
+      LandingPage
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
